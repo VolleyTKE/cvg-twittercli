0e87b177e1c7d95062b31b0c4735bcf6b32e96b4
ccarr <ccarr@technologypartners.net>
2018-09-14 14:57:20 -0500

    node cli for taking a API key and API secret

--- bin/twittercli.js
+++ bin/twittercli.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log("hello world")
+
+const CredentialManager = require('../lib/credential-manager')
+
+async function main () {
+    const creds = new CredentialManager('twittercli')
+    let [key, secret] = await creds.getKeyAndSecret()
+    console.log(key, secret)
+}
+
+main().catch(console.error)
