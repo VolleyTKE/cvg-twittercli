45a9baac5e2564f2f0111c7415407311aeb68fff
VolleyTKE <chris.a.carr@gmail.com>
2018-09-26 14:46:50 -0500

    add code for api pin to configure

--- commands/configure.js
+++ commands/configure.js
@@ -1,6 +1,9 @@
+const querystring = require('querystring')
 const inquirer = require('inquirer')
+const opn = require('opn')
 const CredentialManager = require('../lib/credential-manager')
 const util = require('../lib/util')
+const Twitter = require('../lib/twitter')
 
 const configure = {
     async consumer (name) {
@@ -10,6 +13,39 @@ const configure = {
             {type: 'password', name: 'secret', message: 'Enter your API Secret', validate: util.notEmpty}
         ])
         await creds.storeKeyAndSecret('apiKey', answers.key, answers.secret)
+    }, 
+    async account (name) {
+        let creds = new CredentialManager(name)
+        var [apiKey, apiSecret] = await creds.getKeyAndSecret('apiKey')
+        let twitter = new twitter(apiKey, apiSecret)
+        let response = querystring.parse(await twitter.post('oauth/request_token'))
+        twitter.setToken(response['oauth_token'], response['oauth_token_secret'])
+        await inquirer.prompt({
+            type: 'input',
+            message: 'Press enter to open Twitter in your default browser to authorize access',
+            name: ''
+        })
+
+        opn(`${twitter.baseUrl}oauth/authorize?oauth_token=${response['oauth_token']}`)
+        let answers = await inquirer.prompt({
+            type: 'input',
+            message: 'Enter the PIN provided by Twitter',
+            name: 'pin',
+            validate: util.notEmpty
+        }) 
+
+        let tokenResponse = querystring.parse(
+            await twitter.post('oauth/access_token', `oauth_verifier=${answers['pin']}`)
+        )
+        twitter.setToken(tokenResponse['oauth_token'], tokenResponse['oauth_token_secret'])
+
+        let verifyResponse = await twitter.get('1.1/account/verify_credentials.json')
+        await creds.storeKeyAndSecret(
+            'accountToken',
+            tokenResponse['oauth_token'],
+            tokenResponse["oauth_token_secret"]
+        )
+        console.log(`Account "${verifyResponse['screen_name']}" successfully added`)
     }
 }
 
